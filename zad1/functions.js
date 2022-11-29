1ae776b556bb8d7475a95731ea8de050132d0b73
Witold Weiner <intreks489@gmail.com>
2022-11-29 09:06:23 +0100

    function.js created

---
+++ zad1/functions.js
@@ -0,0 +1,15 @@
+function isOdd(x){
+    return (x%2 == 1 ) 
+}
+const isEven = (x) => {
+   return (x%2 == 0)
+}
+
+console.log(isOdd(10))
+
+console.log(isOdd(11))
+
+
+console.log(isEven(10))
+
+console.log(isEven(11))
